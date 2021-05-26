93ae977c77436c0fae1a27d0ca9765782925e080
Fajar Inti Persada <fajar.inti@ti.ukdw.ac.id>
2021-05-26 10:46:48 +0700

    fix add notes success  message

--- src/handler.js
+++ src/handler.js
@@ -14,7 +14,7 @@ const addNoteHandler = (request, h) => {
 
   notes.push(newNote)
 
-  const isSuccess = notes.filter((note) => notes.id === id).length > 0
+  const isSuccess = notes.filter((note) => note.id === id).length > 0
 
   if (isSuccess) {
     const response = h.response({
